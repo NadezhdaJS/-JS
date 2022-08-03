c393f5c2d7f7c6f9433abe6174387cd1766f0b3f
NadezhdaJS <94574119+NadezhdaJS@users.noreply.github.com>
2022-08-03 15:11:53 +0400

    add: loop 1

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@
 <!-- В первом подъезде квартиры с 1 по 20. Во втором с 21 по 48. В третьем с 49 по 90. Пользователь вводит номер квартиры. Программа должна указать в каком подъезде находится данная квартира.
 
 Программа должна учитывать вариант ввода чисел за пределами диапазона 1..90. --> */
-let flat = 34;
+/* let flat = 34;
 if (flat >= 1 && flat <= 20) {
     console.log(`Квартира ${flat} находится в первом подъезде`)
 } else if (flat >= 21 && flat <= 48) {
@@ -13,4 +13,20 @@ else if (flat >= 49 && flat <= 90) {
     console.log(`Квартира ${flat} находится в третьем подъезде`)
 } else {
     console.log(`Такой квартиры нет в этом доме`)
+} */
+
+
+let array = [1, 2, 3, 4, 5, 6, 7]
+for (let index = 0; index <= 7; index++) {
+
+    if (index === 0) {
+        array[index] = console.log('Introduction')
+    }
+    else if (index === array.length) {
+        array[index] = console.log('Conclusion')
+    }
+    else {
+        array[index] = console.log('Chapter ' + index)
+    }
+
 }
