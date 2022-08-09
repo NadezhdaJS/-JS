aa03b111881a944251249c3e39dc718eb17f2c8c
NadezhdaJS <94574119+NadezhdaJS@users.noreply.github.com>
2022-08-09 15:18:01 +0400

    add: loop 23

--- scripts.js
+++ scripts.js
@@ -248,3 +248,10 @@ for (let i = 1; i <= n; i++) {
     console.log(i);
   }
 } */
+
+// loop 23
+//Using a for loop output the elements in reverse order
+/* let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+for (let i = arr.length - 1; i >= 0; i--) {
+  console.log(arr[i]);
+} */
